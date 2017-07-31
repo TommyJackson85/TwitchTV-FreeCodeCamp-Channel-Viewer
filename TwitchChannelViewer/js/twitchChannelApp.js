6f24015f31d356c5d87f64551b367608e11ca6a6
ThomasJ85 <tom.jackson50@gmail.com>
2017-07-31 17:53:17 +0100

    Cleaned up project.
    Removed unecesary code from SCSS and Javascript files. Removed Some
    file folders as well.

--- TwitchChannelViewer/js/twitchChannelApp.js
+++ TwitchChannelViewer/js/twitchChannelApp.js
@@ -5,9 +5,6 @@ $(document).ready(function() {
   var apiLink = "https://wind-bow.glitch.me/twitch-api"; //base link for api.
   var streamsLinkFCC = apiLink + "/streams/freecodecamp";
   // console.log(streamsLinkFCC);
-  
-  var followerLinkChannel;
-  var followerLinkStream;
   var usersChannels = apiLink + "/users/freecodecamp/follows/channels/";
   //usersChannels links to the list of channels of FCC's followers
   //console.log(usersChannels);
@@ -15,83 +12,81 @@ $(document).ready(function() {
   //console.log(streamsLink);
 
   //FreeCodeCamp link
-  $.getJSON(streamsLinkFCC, function(data1) {
+$.getJSON(streamsLinkFCC, function(data1) {
     if (data1.stream === null) {
       $("#linkFCC").append("<a href='https://www.twitch.tv/freecodecamp'>FCC's Twitch Channel is currently OFFLINE</a>");
     } else {
       $("#linkFCC").append("<a href='https://www.twitch.tv/freecodecamp'>FCC's Twitch Channel is currently ONLINE</a>");
-    }
-  });
+    } 
+});
 
-  $.getJSON(usersChannels, function(data2) {
+$.getJSON(usersChannels, function(data2) {
     //other users and what they are streaming
-    for (var i = 0; i < data2.follows.length; i++) {
-      var eachFollower = data2.follows[i].channel.display_name;
-      followers.push(eachFollower)
+    //this will add new followers to the the followers array above.
+      for (var i = 0; i < data2.follows.length; i++) {
+        var eachFollower = data2.follows[i].channel.display_name;
+        followers.push(eachFollower)
       // console.log(followers);
-    }
+      }
 
-    for (var i = 0; i < followers.length; i++) {
-      var followersStream = apiLink + "/streams/" + followers[i];
-      //each follower's streams api link.. if needed it, the channels api link can be connected to through the streams api.
-     // console.log(followersStream);
+      for (var i = 0; i < followers.length; i++) {
+      var followerStream = apiLink + "/streams/" + followers[i];
+      //each follower's streams api link.. the channels api link can be connected to through the streams api.
+     //console.log(followersStream);
 
-      $.getJSON(followersStream).done(function(live) {
+      $.getJSON(followerStream).done(function(live) {
         var status;
         var name;
         var logo;
-        if (live.stream != null) {
-          // if function for users who are currently live
-          status = live.stream.channel.status;
+        
+          if (live.stream != null) {
+          //users who are currently live
+              status = live.stream.channel.status;
 
-             if (status.length > 4) {
-          console.log(5 * 10); // just for reference in console.
-         status =  "'" + status.slice(0, 38) + "...'";
-        }
+            if (status.length > 4) {
+               status =  "'" + status.slice(0, 38) + "...'"; //shortens link lenght for live channels
+            }
 
-          name = live.stream.channel.display_name;
-          if(live.stream.channel.logo === null){
-         logo = "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-6.jpg"; //gives a placeholder logo if channel doesnt have a logo
-        } else { logo = live.stream.channel.logo; }
-          //console.log(logo)
-          $("#online").append("<div class='online'><img src='" + 
-          logo + "' class='onlineLogo'><h6>" + name + " is Streaming:<br /> <a href='https://www.twitch.tv/" + name + "'>" + status + "</h6></a></h6></div>"); // appends status update
-        };
+            name = live.stream.channel.display_name;
+            if(live.stream.channel.logo === null){
+              logo = "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-6.jpg"; //gives a placeholder logo if channel doesnt have a logo
+            } 
+            else { logo = live.stream.channel.logo; 
+              }
+              //console.log(logo)
+            $("#online").append("<div class='online'><img src='" + logo + "' class='onlineLogo'><h6>" + name + " is Streaming:<br /> <a href='https://www.twitch.tv/" + name + "'>" + status + "</h6></a></h6></div>"); // appends status update
+          };
         
         if(live.stream === null) {
-          //if function for users not live of who dont exist      
-      var newAPIChannel = live._links.channel;
-    newAPIChannel = apiLink + "/channels/" + newAPIChannel.slice(38);
+        //users NOT live or who dont exist      
+          var newAPIChannel = live._links.channel;
+          newAPIChannel = apiLink + "/channels/" + newAPIChannel.slice(38);
           console.log(newAPIChannel);
           //newAPIChannel is a replacement of the channel link in the streams API because the other link wont work without the apiLink variable..
-      $.getJSON(newAPIChannel).done(function(offline) {
-        if (offline.error) {
-          // this if function is for channel users that dont exist anymore
-          logo = "http://blog.extreme-advice.com/wp-content/uploads/2013/01/error.png";
-          // specific logo placeholder for non existant users
-          status = offline.error;
-          name = offline.message;
-          $("#nonUsers").append("<div class='error'><img src='" + logo + "' class='offlineLogo'><div class='offlineStat'><h6>" + name + "</h6></div></div>");
-         
-        } else {
-          //else function for the other users that do exist but arent online
-          name = offline.display_name;
-          if (offline.logo === null){
-         logo = "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-6.jpg"; // placeholder for users without logo
-        } else { 
-            logo = offline.logo; 
-         // console.log(logo)
-               }
+          $.getJSON(newAPIChannel).done(function(offline) {
           
-          $("#offline").append("<div class='offline'><img src='" + logo + "' class='offlineLogo'><div class='status'><h6>" + name + " is currently <a href='https://www.twitch.tv/" + name + "'>OFFLINE</a></h6></div></div>");
-                  
-          //"<div class='row'><div class='span12'><div class='online col-xs-12 col-sm-12'><img src='" + logo + "' class='onlineLogo col-xs-6 col-sm-6'><h6>" + name + "</h6><h6>" + status + "</h6></div></div></div>"
+          if (offline.error) {
+          // channel users that dont exist anymore
+              logo = "http://blog.extreme-advice.com/wp-content/uploads/2013/01/error.png";
+              // specific logo placeholder for non existant users
+              status = offline.error;
+              name = offline.message;
+              $("#nonUsers").append("<div class='error'><img src='" + logo + "' class='offlineLogo'><div class='offlineStat'><h6>" + name + "</h6></div></div>");
+          } else {
+          //users that do exist but arent online
+            name = offline.display_name;
+            if (offline.logo === null){
+              logo = "http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-6.jpg"; // placeholder for users without logo
+            } 
+            else { 
+              logo = offline.logo;
+            }
+            $("#offline").append("<div class='offline'><img src='" + logo + "' class='offlineLogo'><div class='status'><h6>" + name + " is currently <a href='https://www.twitch.tv/" + name + "'>OFFLINE</a></h6></div></div>");
         };
 
-      });
-        };
-      }); 
-    };
-      
-  });
+});
+};
+}); 
+};  
+});
 });
